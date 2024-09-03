1a51d54ecbda28f79c926083171e578e09f27408
Sid Ramesh <sidharth.ramesh@tigeranalytics.com>
2024-09-03 19:55:12 -0400

    chore: Update file paths and table names for data ingestion and creation

--- ingest/pandas_ingest.py
+++ ingest/pandas_ingest.py
@@ -7,7 +7,7 @@ def load_db_config(config_path):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
-db_config_path = '/home/sid/Desktop/copilot/ingest/params.yaml'  # Path to the YAML config file
+db_config_path = '/home/sid/Desktop/dataengg101/ingest/params.yaml'  # Path to the YAML config file
 db_params = load_db_config(db_config_path)['pandas']
 db_user = db_params['user']
 db_password = db_params['password']
@@ -15,8 +15,8 @@ db_database = db_params['database']
 db_ip = db_params['host']
 
 # Replace these variables with your actual file path and database credentials
-csv_file_path = '/home/sid/Desktop/copilot/data/customers.csv'
-table_name = 'landing_customers'
+csv_file_path = '/home/sid/Desktop/dataengg101/data/sales.csv'
+table_name = 'landing_sales'
 
 # Read CSV file
 df = pd.read_csv(csv_file_path)
@@ -25,6 +25,6 @@ df = pd.read_csv(csv_file_path)
 engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_ip}/{db_database}')
 
 # Ingest CSV data into PostgreSQL database
-df.to_sql(table_name, engine, if_exists='replace', index=False)  # Use 'append' to add data to an existing table
+df.to_sql(table_name, engine, if_exists='append', index=False)  # Use 'append' to add data to an existing table
 
 print('Loaded')
--- table/Orders.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Orders (
-    order_id SERIAL PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    total_amount DECIMAL,
-    status VARCHAR(255),
-    FOREIGN KEY (customer_id) REFERENCES landing_customers(customer_id)
-);
